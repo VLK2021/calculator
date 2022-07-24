26f7f971ec0c171c451596d1af09cc008549e97d
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-07-24 17:35:36 +0300

    add +/-

--- src/App.js
+++ src/App.js
@@ -1,59 +1,75 @@
 import './App.css';
+
 import {useState} from "react";
 
 function App() {
-
     let [result, setResult] = useState('');
 
-    const allClear = e => {
-        setResult('');
-    }
+    const clearOne = e => {
+        setResult(result.slice(0, result.length - 1));
+    };
 
     const getNumber = e => {
+        if (result.includes('.') && e.target.innerText === '.') return;
         setResult(result.concat(e.target.name));
-    }
+    };
 
     const getResult = e => {
-        setResult(eval(result).toString());
-    }
+        try {
+            setResult(eval(result).toString());
+        } catch (err) {
+            setResult('error');
+        }
+    };
 
     const getOff = e => {
         setResult('');
-    }
+    };
+
+    const getPlusMinus = () => {
+        if (result.charAt(0) === '-') {
+            setResult(result.substring(1));
+        } else {
+            setResult('-' + result);
+        }
+    };
 
 
     return (
-        <>
+        <div className={'container'}>
             <div className="App">
                 <h1>Calculator</h1>
 
-                <form>
-                    <input className={'inpTab'} type="text" name={'tab'} value={result} placeholder={'0'}/>
-                </form>
+                <div className={'form'}>
+                    <form>
+                        <input className={'inpTab'} type="text" name={'tab'} value={result} placeholder={'0'}/>
+                    </form>
+                </div>
 
                 <div className={'btnBlock'}>
-                    <button name={'C'} className={'btn btnBig'} onClick={allClear}>C</button>
-                    <button name={'OFF'} className={'btn'} onClick={getOff}>OFF</button>
-                    <button name={'/'} className={'btn'} onClick={getNumber}>/</button>
+                    <button name={'C'} className={'btn altBtn'} onClick={clearOne}>C</button>
+                    <button name={'CLEAR'} className={'btn altBtn'} onClick={getOff}>CLEAR</button>
+                    <button name={'C'} className={'btn altBtn'} onClick={getPlusMinus}>+/-</button>
+                    <button name={'/'} className={'btn altBtn'} onClick={getNumber}>&divide;</button>
                     <button name={'7'} className={'btn'} onClick={getNumber}>7</button>
                     <button name={'8'} className={'btn'} onClick={getNumber}>8</button>
                     <button name={'9'} className={'btn'} onClick={getNumber}>9</button>
-                    <button name={'*'} className={'btn'} onClick={getNumber}>*</button>
+                    <button name={'*'} className={'btn altBtn'} onClick={getNumber}>*</button>
                     <button name={'4'} className={'btn'} onClick={getNumber}>4</button>
                     <button name={'5'} className={'btn'} onClick={getNumber}>5</button>
                     <button name={'6'} className={'btn'} onClick={getNumber}>6</button>
-                    <button name={'+'} className={'btn'} onClick={getNumber}>+</button>
+                    <button name={'+'} className={'btn altBtn'} onClick={getNumber}>+</button>
                     <button name={'1'} className={'btn'} onClick={getNumber}>1</button>
                     <button name={'2'} className={'btn'} onClick={getNumber}>2</button>
                     <button name={'3'} className={'btn'} onClick={getNumber}>3</button>
-                    <button name={'-'} className={'btn'} onClick={getNumber}>-</button>
+                    <button name={'-'} className={'btn altBtn'} onClick={getNumber}>-</button>
                     <button name={'0'} className={'btn'} onClick={getNumber}>0</button>
                     <button name={'00'} className={'btn'} onClick={getNumber}>00</button>
                     <button name={'.'} className={'btn'} onClick={getNumber}>.</button>
-                    <button name={'='} className={'btn'} onClick={getResult}>=</button>
+                    <button name={'='} className={'btn altBtn'} onClick={getResult}>=</button>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
